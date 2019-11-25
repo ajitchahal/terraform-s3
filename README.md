c54c63b9205e93b060cefffe3e970c72e2a675f9
ajit chahal <ajit.singh.chahal@sap.com>
2019-11-25 09:48:53 +0100

    parsing done

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # terraform-s3
 A cli to store and retrieve tfstate to/from S3
+
+##
+comand line flags
+
+[blog about cmd flags](https://golangexample.com/generate-flags-by-parsing-structures/)
+
+[git repo of the above project](https://github.com/octago/sflags/)
+
+[get cmd line vars from env variables](https://github.com/jamiealquiza/envy)
+
+# Note: still not a proper solution of
+
+get variable from ENV_variable if set, then from command line args anf parse to a structure.
+
+## Replacing password
+
+option 1
+```go
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+func main() {
+	re := regexp.MustCompile(`\bsecret_\w+":"\w+",$`)
+	fmt.Println(re.ReplaceAllString(`"secret_master_password":"abcdefgh3242",`, "secret_master_password\":\"pass\""))
+}
+
+		re := regexp.MustCompile(`(?m)\bsecret_\w+":( |\t)*"\w+",$`)
+
+```
+option 2
+```go
+var or = `
+ "iam_roles": null,
+            "id": "abntest-service",
+            "kms_key_id": "arn:aws:kms:us-east-1:dd:key/db",
+            "master_password": "secret_abc1234_end",
+            "master_username": "masteruser",
+            "port": 5432,`
+
+	re := regexp.MustCompile(`\bsecret_\w+_end`)
+	fmt.Println(re.ReplaceAllString(or, "pass"))
+```
--- main.go
+++ main.go
@@ -1,21 +1,91 @@
 package main
 
 import (
+	"flag"
+	"time"
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/octago/sflags/gen/gflag"
+	"io/ioutil"
 	"log"
 	"os"
+	"regexp"
 )
 
-func main() {
-	//listBucketItems()
-	//uploadToBucket()
-	downloadFromBucket()
+type awsS3 struct {
+	Bucket    string `desc:"S3 bucket name"`
+	FileName  string `desc:"File to upload or download"`
+	Operation string `desc:"up|dwn upload or download file" flag:"op"`
+	Region    string `desc:"AWS region the bucket is in e.g us-east-1"`
 }
-func downloadFromBucket(){
+type config struct {
+	S3 awsS3
+}
+
+var cfg = &config{}
+
+func parseCmdLineArgs() {
+	err := gflag.ParseToDef(cfg) //parseEnv
+	if err != nil {
+		log.Fatalf("err: %v", err)
+	}
+
+	flag.Parse()
+	log.Println(cfg)
+	fmt.Println("tail:", flag.Args())
+}
+func main() {
+	// parseCmdLineArgs() //go run main.go --help
+
+	// switch cfg.S3.Operation {
+	// case "up":
+	// 	downloadFromBucket()
+	// case "down":
+	// 	downloadFromBucket()
+	// }
+	buff, err := ioutil.ReadFile("terraform.tfstate")
+	if err != nil {
+		log.Fatal(err)
+	}
+	//log.Println(string(buff))
+	fileContent := string(buff)
+	replacePasswordsWithPlaceHolders(fileContent)
+	time.Sleep(2 * time.Second)
+	buff, err = ioutil.ReadFile("terraform.tfstate")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fileContent = string(buff)
+
+	replacePlaceHoldersWithPasswords(fileContent, "ajit")
+}
+func replacePasswordsWithPlaceHolders(fileContent string) {
+	secretToReplace := "master_password"
+	regEx := fmt.Sprintf(`(?m)\b%s":( |\t)*"\w+",$`, secretToReplace)
+
+	fmt.Println(regEx)
+	re := regexp.MustCompile(regEx)
+
+	template := "%s\": \"%s\","
+	templatedStr := fmt.Sprintf(template, secretToReplace, "secret_" + secretToReplace + "_end")
+	fileContent = re.ReplaceAllString(fileContent, templatedStr)
+
+	//fmt.Println(fileContent)
+	ioutil.WriteFile("terraform.tfstate", []byte(fileContent), 0644)
+}
+func replacePlaceHoldersWithPasswords(fileContent string, secret string) {
+	secretToReplace := "master_password"
+	regEx := fmt.Sprintf(`\bsecret_%s_end`, secretToReplace)
+
+	fmt.Println(regEx)
+	re := regexp.MustCompile(regEx)
+	fileContent = re.ReplaceAllString(fileContent, secret)
+	ioutil.WriteFile("terraform.tfstate", []byte(fileContent), 0644)
+}
+func downloadFromBucket() {
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String("us-east-1")},
 	)
@@ -24,11 +94,11 @@ func downloadFromBucket(){
 
 	filename := "README.md"
 	file, err := os.Create(filename)
-    if err != nil {
-        exitErrorf("Unable to open file %q, %v", err)
-    }
+	if err != nil {
+		exitErrorf("Unable to open file %q, %v", err)
+	}
 
-    defer file.Close()
+	defer file.Close()
 	downloader := s3manager.NewDownloader(sess)
 
 	numBytes, err := downloader.Download(file,
@@ -39,7 +109,7 @@ func downloadFromBucket(){
 	if err != nil {
 		exitErrorf("Unable to download item %q, %v", filename, err)
 	}
-	
+
 	fmt.Println("Downloaded", file.Name(), numBytes, "bytes")
 }
 func uploadToBucket() {
@@ -61,14 +131,14 @@ func uploadToBucket() {
 
 	_, err = uploader.Upload(&s3manager.UploadInput{
 		Bucket: aws.String(bucket),
-		Key: aws.String(filename),
-		Body: file,
+		Key:    aws.String(filename),
+		Body:   file,
 	})
 	if err != nil {
 		// Print the error and exit.
 		exitErrorf("Unable to upload %q to %q, %v", filename, bucket, err)
 	}
-	
+
 	fmt.Printf("Successfully uploaded %q to %q\n", filename, bucket)
 }
 func listBucketItems() {
